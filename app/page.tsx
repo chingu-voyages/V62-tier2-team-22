10cfeb0ed889656dc57bd39016577bfa95f6b17c
martiNik2 <anas88893142@gmail.com>
2026-09-14 21:21:52 +0300

    fix name and add example in app home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { AuthStatus, GoogleSignInButton } from "@/components/AuthButton";
-import Image from "next/image";
+import { AuthStatus} from "@/components/Authentication/AuthButton";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 		<AuthStatus/>
-		<GoogleSignInButton/>
     </div>
   );
 }
