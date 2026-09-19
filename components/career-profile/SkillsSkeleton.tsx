25beb3725066b052addbfc354c88a5b3a529da89
martiNik2 <anas88893142@gmail.com>
2026-09-20 00:34:39 +0300

    add skeletons for pending skills

---
+++ components/career-profile/SkillsSkeleton.tsx
@@ -0,0 +1,11 @@
+export default function Skeletons(){
+	return (
+        <div className="flex flex-wrap gap-2.5 mt-2">
+			<div className="px-13 py-4 rounded-full text-xs font-medium border flex items-center gap-1.5 bg-slate-200 border-slate-200 text-slate-600 hover:border-slate-300 animate-pulse"></div>
+			<div className="px-12 py-4 rounded-full text-xs font-medium border flex items-center gap-1.5 bg-slate-200 border-slate-200 text-slate-600 hover:border-slate-300 animate-pulse"></div>
+			<div className="px-10 py-4 rounded-full text-xs font-medium border flex items-center gap-1.5 bg-slate-200 border-slate-200 text-slate-600 hover:border-slate-300 animate-pulse"></div>
+			<div className="px-9 py-4 rounded-full text-xs font-medium border flex items-center gap-1.5 bg-slate-200 border-slate-200 text-slate-600 hover:border-slate-300 animate-pulse"></div>
+			<div className="px-12 py-4 rounded-full text-xs font-medium border flex items-center gap-1.5 bg-slate-200 border-slate-200 text-slate-600 hover:border-slate-300 animate-pulse"></div>
+		</div>
+	)
+}
